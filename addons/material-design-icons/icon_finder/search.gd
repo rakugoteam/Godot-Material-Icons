a5eab38ba97a6d74c05d3cf28cfbd637437b65f6
Jeremi Biernacki <jeremi360@gmail.com>
2021-09-16 14:41:01 +0200

    move "Search Material Icons" to Godot's **Tools** menu

--- addons/material-design-icons/icon_finder/search.gd
+++ addons/material-design-icons/icon_finder/search.gd
@@ -7,13 +7,13 @@ onready var grid := get_node(grid_node_path) as GridContainer
 func _ready():
 	connect("text_changed", self, "_on_text_changed")
 
-func _on_text_changed(text):
-	if text != "":
+func _on_text_changed(new_text):
+	if new_text == '':
 		for b in grid.get_children():
-			if text in b.name:
+			b.show()
+	else:
+		for b in grid.get_children():
+			if new_text in b.name:
 				b.show()
 			else:
 				b.hide()
-	else:
-		for b in grid.get_children():
-			b.show()
--- addons/material-design-icons/plugin.gd
+++ addons/material-design-icons/plugin.gd
@@ -1,12 +1,12 @@
 tool
 extends EditorPlugin
-var button = preload("tool_button/ToolButton.tscn")
+var icon_search : WindowDialog
+var menu_item := ["Search Material Icons", "popup_centered", Vector2(450, 400)]
 
 func _enter_tree():
-	button = button.instance()
-	add_control_to_container(CONTAINER_TOOLBAR, button)
-	var p = button.get_parent()
-	p.move_child(button, 1)
+	icon_search = preload("icon_finder/IconFinder.tscn").instance()
+	add_control_to_container(CONTAINER_TOOLBAR, icon_search)
+	add_tool_menu_item(menu_item[0], icon_search, menu_item[1], menu_item[2])
 
 	add_custom_type(
 		"MaterialIcon", "Label",
@@ -23,4 +23,4 @@ func _enter_tree():
 func _exit_tree():
 	remove_custom_type("MaterialIcon")
 	remove_custom_type("MaterialButton")
-	remove_control_from_container(CONTAINER_TOOLBAR, button)
+	remove_control_from_container(CONTAINER_TOOLBAR, icon_search)
