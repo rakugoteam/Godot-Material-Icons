aa8e7d564da5a46f497752a25d38f47fa5f15ac0
Jeremi Biernacki <jeremi360@gmail.com>
2023-07-03 18:17:32 +0200

    port search.gd to new GDScript

--- addons/material-design-icons/icon_finder/search.gd
+++ addons/material-design-icons/icon_finder/search.gd
@@ -1,19 +1,20 @@
-tool
+@tool
 extends LineEdit
 
-export var grid_node_path : NodePath
-onready var grid := get_node(grid_node_path) as GridContainer
+@export var grid_node_path : NodePath
+@onready var grid := get_node(grid_node_path) as GridContainer
 
 func _ready():
-	connect("text_changed", self, "_on_text_changed")
+	text_changed.connect(_on_text_changed)
 
 func _on_text_changed(new_text):
 	if new_text == '':
 		for b in grid.get_children():
 			b.show()
-	else:
-		for b in grid.get_children():
-			if new_text in b.name:
-				b.show()
-			else:
-				b.hide()
+			return
+	
+	for b in grid.get_children():
+		if new_text in b.name:
+			b.show()
+		else:
+			b.hide()
