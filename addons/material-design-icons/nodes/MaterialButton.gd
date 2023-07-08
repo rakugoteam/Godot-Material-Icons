fc9160027ba8d85d362ddf3f0759ca002ed58f1b
Jeremi Biernacki <jeremi360@gmail.com>
2023-07-08 15:16:53 +0200

    fix Material Icons Nodes by adding getters

--- addons/material-design-icons/nodes/MaterialButton.gd
+++ addons/material-design-icons/nodes/MaterialButton.gd
@@ -3,24 +3,31 @@
 extends Button
 class_name MaterialButton
 
-@export var icon_name := "image-outline":
+var _icon_name := "image-outline"
+var _icon_size := 16
+
+@export var icon_name : String:
 	set(value):
 		_set_icon_name(value)
+	get:
+		return _icon_name
 
 @export_range(16, 128, 1)
-var icon_size := 16:
+var icon_size : int:
 	set(value):
 		_set_icon_size(value)
-
-var font: FontFile
+	get:
+		return _icon_size
 
 func _ready():
 	clip_text = false
-	font = MaterialIconsDB.font
+	var font := MaterialIconsDB.font
 	set("theme_override_fonts/font", font)
 
 func _set_icon_name(value: String):
+	_icon_name = value
 	text = MaterialIconsDB.get_icon_char(value)
 
 func _set_icon_size(value: int):
+	_icon_size = value
 	set("theme_override_font_sizes/font_size", value)
--- addons/material-design-icons/nodes/MaterialIcon.gd
+++ addons/material-design-icons/nodes/MaterialIcon.gd
@@ -4,24 +4,31 @@
 extends Label
 class_name MaterialIcon
 
-@export var icon_name := "image-outline":
+var _icon_name := "image-outline"
+var _icon_size := 16
+
+@export var icon_name : String:
 	set(value):
 		_set_icon_name(value)
+	get:
+		return _icon_name
 
 @export_range(16, 128, 1)
-var icon_size := 16:
+var icon_size : int:
 	set(value):
 		_set_icon_size(value)
-
-var font: FontFile
+	get:
+		return _icon_size
 
 func _ready():
 	clip_text = false
-	font = MaterialIconsDB.font
+	var font := MaterialIconsDB.font
 	set("theme_override_fonts/font", font)
 
 func _set_icon_name(value: String):
+	_icon_name = value
 	text = MaterialIconsDB.get_icon_char(value)
 
 func _set_icon_size(value: int):
+	_icon_size = value
 	set("theme_override_font_sizes/font_size", value)
